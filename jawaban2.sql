d77184f495b236deea56368662e0cbd6245eb7ad
Mawanrs <150999823+Mawanrs@users.noreply.github.com>
2023-11-16 14:02:04 +0700

    jawaban2.sql

--- jawaban2.sql
+++ jawaban2.sql
@@ -4,7 +4,8 @@ INSERT INTO Mahasiswa (id,nama_depan, nama_belakang, address, phone, fakultas_id
 INSERT INTO Dosen (id,kode_dosen, nama_depan, nama_belakang, fakultas_id) VALUES
 		(1, '1234', 'Jefry' , 'Sunupurwa Asri', 1);
 		
-INSERT INTO Fakultas (id, nama_fakultas) VALUES (1, 'Teknik Informatika');
+INSERT INTO Fakultas (id, nama_fakultas) VALUES
+		(1, 'Teknik Informatika');
 		
 INSERT INTO Mata_Kuliah (id, nama_mata_kuliah, fakultas_id) VALUES
-(1, 'Basis Data', 1);
+		(1, 'Basis Data', 1);
